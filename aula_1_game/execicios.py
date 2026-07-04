bbc9e1c5ec374937c3359f5a8439d0da6fe178ee
AndreMicheletti <andreluizmtmicheletti>
2026-07-04 16:32:50 -0300

    :tada: primeira aula

---
+++ aula_1_game/execicios.py
@@ -0,0 +1,20 @@
+# Hello World
+
+# Hello World com variável
+
+# Hello World com numero
+
+# Hello World com input
+
+# Hello World com boolean
+
+# Hello World com if/else
+
+# Hello World com if/else + not
+
+# Hello World com if/else + and
+
+# LINHAS:
+# 29
+# 383
+# 224
